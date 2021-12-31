3ca5ab9929a82196ed74115fbd82a4352e15e7be
bc-ppuente <patrick.puente@bigcommerce.com>
2021-12-31 11:27:37 -0600

    estimate types

--- pages/api/tax/estimate.ts
+++ pages/api/tax/estimate.ts
@@ -1,5 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { EstimateRequest, EstimateResponse } from '../../../types'
 import { taxProviderBasicAuth } from '../../../lib/auth'
+
+// Hard-coded tax rate for demo purposes.
+const taxRate = .0825
 
 export default async function estimate(req: NextApiRequest, res: NextApiResponse) {
     try {
@@ -7,7 +11,10 @@ export default async function estimate(req: NextApiRequest, res: NextApiResponse
             res.status(401).json({ "error": "Unauthorized" })
             return
         }
-        
+        const estimateRequest = req.body as EstimateRequest
+
+
+
         res.status(200).json({ 'Hello': 'world' })
     } catch (error) {
         const { message, response } = error;
---
+++ types/estimate.ts
@@ -0,0 +1,107 @@
+export interface EstimateRequestCustomer {
+    customer_id: string;
+    customer_group_id: string;
+    taxability_code?: string;
+}
+
+export interface EstimateDocumentItemTaxClass {
+    code: string;
+    class_id: string;
+    name: string;
+}
+
+export interface EstimateRequestDocumentPrice {
+    amount: number;
+    tax_inclusive: boolean;
+}
+
+export enum EstimateDocumentItemType {
+    ITEM = "item",
+    WRAPPING = "wrapping",
+    HANDLING = "handling",
+    SHIPPING = "shipping",
+    REFUND = "refund"
+}
+
+export interface EstimateRequestDocumentItem {
+    id: string;
+    item_code?: string;
+    name?: string;
+    price: EstimateRequestDocumentPrice;
+    quantity: number;
+    tax_class: EstimateDocumentItemTaxClass;
+    tax_exempt?: boolean;
+    type: EstimateDocumentItemType;
+    wrapping?: any;
+}
+
+export enum EstimateRequestDocumentAddressType {
+    RESIDENTIAL = "RESIDENTIAL",
+    COMMERCIAL = "COMMERCIAL",
+}
+
+export interface EstimateRequestDocumentAddress {
+    line1?: string;
+    line2?: string;
+    city?: string;
+    region_name?: string;
+    region_code?: string;
+    country_name?: string;
+    country_code?: string;
+    postal_code?: string;
+    company_name?: string;
+    type?: EstimateRequestDocumentAddressType;
+}
+
+export interface EstimateRequestDocument {
+    id: string;
+    billing_address?: EstimateRequestDocumentAddress;
+    destination_address: EstimateRequestDocumentAddress;
+    origin_address: EstimateRequestDocumentAddress;
+    shipping: EstimateRequestDocumentItem;
+    handling: EstimateRequestDocumentItem;
+    items: EstimateRequestDocumentItem[];
+}
+
+export interface EstimateRequest {
+    id: string;
+    currency_code: string;
+    customer: EstimateRequestCustomer;
+    transaction_date: string;
+    documents: EstimateRequestDocument[]
+}
+
+export interface EstimateResponseTaxSummaryItem {
+    name: string;
+    rate: number;
+    amount: number;
+    tax_class: EstimateDocumentItemTaxClass;
+    id: string;
+}
+
+export interface EstimateResponseDocumentPrice {
+    amount_inclusive: number;
+    amount_exclusive: number;
+    tax_rate: number;
+    sales_tax_summary: EstimateResponseTaxSummaryItem[]
+}
+
+export interface EstimateResponseDocumentItem {
+    id: string;
+    price: EstimateResponseDocumentPrice;
+    type: EstimateDocumentItemType;
+    wrapping?: any;
+}
+
+export interface EstimateResponseDocument {
+    id: string;
+    external_id: string;
+    items: EstimateResponseDocumentItem[];
+    shipping: EstimateResponseDocumentItem;
+    handling: EstimateResponseDocumentItem;
+}
+
+export interface EstimateResponse {
+    id: string;
+    documents: EstimateResponseDocument[]
+}
